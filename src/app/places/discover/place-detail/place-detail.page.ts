68a16f4486a818d3a32436bd6f80b4532d7f209d
schurmann <schurms@bigpond.com>
2020-06-15 20:00:32 +1000

    Course content 126

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NavController } from '@ionic/angular';
+import {ModalController, NavController} from '@ionic/angular';
 
 import { PlacesService } from '../../places.service';
 import { Place } from '../../place.model';
+import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
 
 @Component({
   selector: 'app-place-detail',
@@ -12,11 +13,12 @@ import { Place } from '../../place.model';
 })
 export class PlaceDetailPage implements OnInit {
   place: Place;
-  
+
   constructor(
     private navCtrl: NavController,
     private route: ActivatedRoute,
-    private placesService: PlacesService
+    private placesService: PlacesService,
+    private modalCtrl: ModalController
   ) {}
 
   ngOnInit() {
@@ -30,8 +32,20 @@ export class PlaceDetailPage implements OnInit {
   }
 
   onBookPlace() {
-    // this.router.navigateByUrl('/places/tabs/discover');
-    this.navCtrl.navigateBack('/places/tabs/discover');
-    // this.navCtrl.pop();
-  }
+    this.modalCtrl
+        .create({
+          component: CreateBookingComponent,
+          componentProps: {selectedPlace: this.place}
+        })
+        .then(modalEl => {
+          modalEl.present();
+          return modalEl.onDidDismiss();
+        })
+        .then(resultData => {
+          console.log(resultData.data, resultData.role);
+          if (resultData.role === 'confirm') {
+            console.log('BOOKED!');
+          }
+        });
+    }
 }
